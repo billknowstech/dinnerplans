baee94679798a0473c987d48fe2aada26aefca85
billknowstech <washart70@hotmail.com>
2016-07-27 15:18:05 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,10 @@
+var rests = [{
+    name: 'CPK',
+    city: 'Philadelphia',
+    desc: 'Kids love the sunday desert'
+
+}, {
+    name: 'The Pub of Penn Valley',
+    city: 'Lower Merion',
+    desc: 'Local pub. sometimes we walk to it'
+}];
